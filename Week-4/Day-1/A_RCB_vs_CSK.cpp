31308cb6e6a55a97ac8e68e0f269613b7965302d
GUNJON BARUA ARKA <155982830+arkabarua69@users.noreply.github.com>
2024-11-17 10:30:12 +0600

    Add files via upload
    Day-1

---
+++ Week-4/Day-1/A_RCB_vs_CSK.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl endl
+#define cy cout << "YES" << endl
+#define cn cout << "NO" << endl
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    int ans = x - y;
+    (ans >= 18) ? cout << "RCB" << nl : cout << "CSK" << nl;
+}
+/*Code by Gunjon*/
---
+++ Week-4/Day-1/B_Football_Ties.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl endl
+#define cy cout << "YES" << endl
+#define cn cout << "NO" << endl
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << (y % 3) << nl;
+    }
+}
+/*Code by Gunjon*/
