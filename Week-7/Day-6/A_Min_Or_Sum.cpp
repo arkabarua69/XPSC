33a0e7f95d851ce2c4268606af5496d3700bceb5
GUNJON BARUA ARKA <155982830+arkabarua69@users.noreply.github.com>
2024-12-13 21:55:14 +0600

    Add files via upload
    Day-6

---
+++ Week-7/Day-6/A_Min_Or_Sum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl endl
+#define it(_) (_).begin(), (_).end()
+#define cy cout << "YES" << endl
+#define cn cout << "NO" << endl
+#define Speed_code               \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);
+void GunJon()
+{
+
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        ans |= x;
+    }
+    cout << ans << nl;
+}
+int main()
+{
+    Speed_code;
+    int $;
+    cin >> $;
+    while ($--)
+        GunJon();
+}
+/*Code by Gunjon*/
